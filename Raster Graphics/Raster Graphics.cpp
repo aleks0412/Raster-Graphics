d4c77298da98b51e4d043e1b88fd63bd2485373a
Aleks Karabashev <alekskarabashev@gmail.com>
2024-06-23 22:58:15 +0300

    Exception handling in main

--- Raster Graphics/Raster Graphics.cpp
+++ Raster Graphics/Raster Graphics.cpp
@@ -2,8 +2,31 @@
 
 int main()
 {
-	Application app;
-	app.run();
+	try
+	{
+		Application app;
+		app.run();
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (const std::out_of_range& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (const std::bad_alloc& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Unknown error" << std::endl;
+	}
 
     return 0;
 }
