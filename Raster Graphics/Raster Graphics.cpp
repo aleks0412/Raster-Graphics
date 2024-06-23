7660f39d35d9bc13c0a1174628e9afed1a70e7e4
Aleks Karabashev <alekskarabashev@gmail.com>
2024-06-23 18:50:26 +0300

    Create session in main and basic commands

--- Raster Graphics/Raster Graphics.cpp
+++ Raster Graphics/Raster Graphics.cpp
@@ -1,20 +1,61 @@
 ﻿#include <iostream>
+#include "IImageReader.h"
 #include "PPMImageReader.h"
 #include "PPMImageWriter.h"
 #include "NegativeTransform.h"
+#include "DesaturateTransform.h"
+#include "ClockwiseRotationTransform.h"
 #include "SubclassPtr.hpp"
+#include "Session.h"
+#include "Vector.hpp"
+#include "String.h"
 
 int main()
 {
-    PPMImageReader reader;
-    Image image = reader.readImage("test.ppm");
-    std::cout << image.getWidth() << " " << image.getHeight() << std::endl;
-	int imageWidth = image.getWidth();
-	int imageHeight = image.getHeight();
+	bool isAppRunning = true;
+	Vector<Session> sessions;
+	Session session;
+	sessions.pushBack(std::move(session));
+	SubclassPtr<IImageReader> reader = SubclassPtr<IImageReader>(new PPMImageReader());
+	Image image = reader->readImage("test.ppm");
+	sessions[0].addImage(std::move(image));
+	while (isAppRunning)
+	{
+		String command;
+		std::cin >> command;
+		if (command == "exit")
+		{
+			isAppRunning = false;
+		}
+		else if (command == "load")
+		{
+			String imagePath;
+			std::cin >> imagePath;
+			sessions[0].addImage(reader->readImage(imagePath.c_str()));
+		}
+		else if (command == "grayscale")
+		{
+			SubclassPtr<ITransform> desaturateTransform(new DesaturateTransform());
+			sessions[0].addTransform(std::move(desaturateTransform));
+		}
+		else if (command == "negative")
+		{
+			SubclassPtr<ITransform> negativeTransform(new NegativeTransform());
+			sessions[0].addTransform(std::move(negativeTransform));
+		}
+		else if (command == "clockwise")
+		{
+			SubclassPtr<ITransform> clockwiseRotationTransform(new ClockwiseRotationTransform());
+			sessions[0].addTransform(std::move(clockwiseRotationTransform));
+		}
+		else if (command == "undo")
+		{
+			sessions[0].popTransform();
+		}
+	}
 	PPMImageWriter writer;
-	SubclassPtr<NegativeTransform> negativeTransform(new NegativeTransform());
-	negativeTransform->transformImage(image);
-	writer.writeImage("testNegative.ppm", image);
+	sessions[0].transformImages();
+	writer.writeImage("testResult.ppm", sessions[0].getImage(0));
 
     return 0;
 }
